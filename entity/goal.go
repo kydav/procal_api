212083027ba1bb56c87584a3b33710fcfa30ed8d
Kyler Davis <kyler.davis@instride.health>
2025-07-30 21:51:10 -0600

    Update User entity fields and change env file in main.go
    Added BirthDate, CurrentWeight, Height, and Age fields to the User entity. Removed CurrentWeight from Goal entity.

--- entity/goal.go
+++ entity/goal.go
@@ -4,10 +4,9 @@ import "gorm.io/gorm"
 
 type Goal struct {
 	gorm.Model
-	UserID        string `json:"userId" gorm:"type:uuid;not_null;unique"`
-	ProteinGoal   int    `json:"proteinGoal" gorm:"type:int;not_null;"`
-	CalorieGoal   int    `json:"calorieGoal" gorm:"type:int;not_null;"`
-	WeightGoal    int    `json:"weightGoal" gorm:"type:int;not_null;"`
-	CurrentWeight int    `json:"currentWeight" gorm:"type:int;not_null;"`
-	Objective     string `json:"objective" gorm:"type:varchar(50);not_null;"`
+	UserID      string `json:"userId" gorm:"type:uuid;not_null;unique"`
+	ProteinGoal int    `json:"proteinGoal" gorm:"type:int;not_null;"`
+	CalorieGoal int    `json:"calorieGoal" gorm:"type:int;not_null;"`
+	WeightGoal  int    `json:"weightGoal" gorm:"type:int;not_null;"`
+	Objective   string `json:"objective" gorm:"type:varchar(50);not_null;"`
 }
--- entity/user.go
+++ entity/user.go
@@ -1,12 +1,20 @@
 package entity
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	ID        string `json:"id" gorm:"primary_key;type:uuid;default:uuid_generate_v4();not_null;unique"`
-	Email     string `json:"email" gorm:"type:varchar(70);index:user_email_ln_uniq,unique,where:deleted_at is null"`
-	FirstName string `json:"first_name" gorm:"type:varchar(70)"`
-	LastName  string `json:"last_name" gorm:"type:varchar(70)"`
-	IsActive  bool   `json:"is_active"`
+	ID            string    `json:"id" gorm:"primary_key;type:uuid;default:uuid_generate_v4();not_null;unique"`
+	Email         string    `json:"email" gorm:"type:varchar(70);index:user_email_ln_uniq,unique,where:deleted_at is null"`
+	FirstName     string    `json:"first_name" gorm:"type:varchar(70)"`
+	LastName      string    `json:"last_name" gorm:"type:varchar(70)"`
+	BirthDate     time.Time `json:"birth_date" gorm:"type:date"`
+	CurrentWeight int       `json:"current_weight" gorm:"type:int;not_null;"`
+	Height        int       `json:"height" gorm:"type:int;not_null;"`
+	Age           int       `json:"age" gorm:"type:int;not_null;"`
+	IsActive      bool      `json:"is_active"`
 }
